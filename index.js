7a8e92620fee1a8156aff7deac073b3ae984186d
Mark Hamilton <mark@markhamilton.info>
2025-02-18 15:21:04 -0600

    refactor: streamline endpoint initialization

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const APIWrapper = require('./api');
 
 module.exports = function(url, username, password) {
     const api = new APIWrapper(url, username, password);
-  
-    const APIHealth     = require('./endpoints/APIHealth');
-    const Applications  = require('./endpoints/Applications');
-    const Customers     = require('./endpoints/Customers');
-    const Leads         = require('./endpoints/Leads');
-    const Leases        = require('./endpoints/Leases');
-    const Maintenance   = require('./endpoints/Maintenance');
-    const Pricing       = require('./endpoints/Pricing');
-    const Properties    = require('./endpoints/Properties');
-    const PropertyUnits = require('./endpoints/PropertyUnits');
+    const endpoints = [
+        'APIHealth',
+        'Applications',
+        'Customers',
+        'Leads',
+        'Leases',
+        'Maintenance',
+        'Pricing',
+        'Properties',
+        'PropertyUnits'
+    ];
 
-    return {
-        APIHealth     : new APIHealth(api),
-        Applications  : new Applications(api),
-        Customers     : new Customers(api),
-        Leads         : new Leads(api),
-        Leases        : new Leases(api),
-        Maintenance   : new Maintenance(api),
-        Pricing       : new Pricing(api),
-        Properties    : new Properties(api),
-        PropertyUnits : new PropertyUnits(api),
-    };
-  };
+    const result = {};
+    endpoints.forEach(endpoint => {
+        const EndpointClass = require(`./endpoints/${endpoint}`);
+        result[endpoint] = new EndpointClass(api);
+    });
+
+    return result;
+};
